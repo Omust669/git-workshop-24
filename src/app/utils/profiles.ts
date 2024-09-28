bab953fcf10cba77ab7d42ec8d9f72ea5b316d4e
Jeevan nayak <jeevangknayak@gmail.com>
2024-09-28 12:39:35 +0530

    added changes

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
-];
+{
+  username: "Omus669",
+  name: "Jeevan Nayak",
+  quote: "nothing to see here",
+},;
+]
